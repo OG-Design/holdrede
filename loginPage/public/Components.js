c72367d81b24680fb8e021b6708133a78f0d3e8c
og-design <marcus@olsen-graphics.no>
2025-04-08 17:38:56 +0200

    Update Components.js

--- loginPage/public/Components.js
+++ loginPage/public/Components.js
@@ -1,5 +1,15 @@
 console.log("ComponentsScriptRunning");
 document.getElementById('notesAppBtn').addEventListener("click", notesApp)
+
+function showUserName () {
+    let username;
+}
+
+function homeApp () {
+
+}
+
+
 function notesApp () {
     console.log("running: notesApp")
     const getAppRoot = document.getElementById('noteRoot');
